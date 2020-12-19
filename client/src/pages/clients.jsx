d5169adf30602eff81747e4e7056bb9476f6c0af
100l36n1za <100l36n1za@gmail.com>
2020-12-19 16:56:35 +0300

    hot fix 2

--- client/src/pages/clients.jsx
+++ client/src/pages/clients.jsx
@@ -3,21 +3,19 @@ import React, { useEffect, useState } from 'react'
 export const Clients = () => {
 	const [user, setUser] = useState(null)
 
-
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	useEffect(async () => {
 		const response = await fetch('/api/clients')
-
 		const data = await response.json()
-		console.log(data)
+		console.log(data);
 		setUser(data)
 	}, [])
 
 	return user ? (
-		<ul class="collection">
-			{user.map((item, id) => {
+		<ul className="collection">
+			{user.map((item, ind) => {
 				return (
-					<li class="collection-item">{user.name}</li>
+					<li key={ind} className="collection-item">{item.name}</li>
 				)
 			})}
 		</ul>
--- routes/client-routes.js
+++ routes/client-routes.js
@@ -4,6 +4,7 @@ const db = require('../db')
 
 router.get('/', async (req, res) => {
 	const clients = await db.query('SELECT * FROM client')
+	console.log(clients.rows);
 	res.json(clients.rows)
 })
 
