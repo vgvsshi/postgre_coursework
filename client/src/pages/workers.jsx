4fd58adb866dcb9fdb5a7aed8ec795c465e0306b
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-12-20 21:29:15 +0300

    small fix

--- client/src/pages/workers.jsx
+++ client/src/pages/workers.jsx
@@ -16,26 +16,43 @@ export const Workers = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
-	return workers ? (
-		<div className='main' >
-			<div className='main-wrapper'>
-				<div className='main-title'>Рабочие</div>
+	return workers.length !== 0 ?
+		(
+			<div className='main' >
+				<div className='main-wrapper'>
+					<div className='main-title'>Рабочие</div>
 
-				<Link style={{ marginBottom: '20px' }} className='btn waves-effect waves-light teal darken-3' to='/admin/add-worker'>Добавить рабочего</Link>
+					<Link style={{ marginBottom: '20px' }} className='btn waves-effect waves-light teal darken-3' to='/admin/add-worker'>Добавить рабочего</Link>
 
-				<ul style={{ marginTop: '10px', width: '100%' }} className="collection">
-					{workers.map((item, id) => {
-						return (
-							<Link key={id} className="collection-item" to={{
-								pathname: `/admin/change-worker/${item.id}`,
-								state: {
-									id: item.id
-								}
-							}}>{item.name}</Link>
-						)
-					})}
-				</ul>
+					<ul style={{ marginTop: '10px', width: '100%' }} className="collection">
+						{workers.map((item, id) => {
+							return (
+								<Link key={id} className="collection-item" to={{
+									pathname: `/admin/change-worker/${item.id}`,
+									state: {
+										id: item.id
+									}
+								}}>{item.name}</Link>
+							)
+						})}
+					</ul>
+				</div>
 			</div>
-		</div>
-	) : (null)
+		)
+		:
+		(
+			<div className='main'>
+				<div style={{ marginTop: '100px' }} className="preloader-wrapper big active">
+					<div className="spinner-layer spinner-green-only">
+						<div className="circle-clipper left">
+							<div className="circle"></div>
+						</div><div className="gap-patch">
+							<div className="circle"></div>
+						</div><div className="circle-clipper right">
+							<div className="circle"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
 }
